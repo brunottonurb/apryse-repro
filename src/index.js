9d54057f7e1bbbc4e303dacd8fcd2b1b99da70d8
Bruno Péjac <bruno.pejac@procilon.de>
2025-10-13 08:52:45 +0200

    Try to recreate EPB UI with v11

--- src/index.js
+++ src/index.js
@@ -1,99 +1,93 @@
 import WebViewer from '@pdftron/webviewer'
+import ui from './ui2';
 
 const element = document.getElementById('viewer');
 
 const webViewerInstance = await WebViewer.Iframe(
     {
-        ui: 'legacy',
         path: '/lib/webviewer',
         // licenseKey,
         fullAPI: true,
-        disabledElements: [
-            'downloadButton',
-            'printButton',
-            'highlightToolGroupButton',
-            'underlineToolGroupButton',
-            'strikeoutToolGroupButton',
-            'squigglyToolGroupButton',
-            'stickyToolGroupButton',
-            'freeTextToolGroupButton',
-            'shapeToolGroupButton',
-            'freeHandToolGroupButton',
-            'freeHandHighlightToolGroupButton',
-            'eraserToolButton',
-            'leftPanelButton',
-            'searchButton',
-            'menuButton',
-            'toggleNotesButton',
-            'ribbonsDropdown',
-            // 'selectToolButton',
-            'toolsHeader',
-            'ribbons',
-
-            // The dropdown menu for the annotation tools, when you select an annotation
-            // 'annotationCommentButton',
-            // 'annotationStyleEditButton',
-            // 'linkButton',
-            'annotationPopup', // disable the whole popup/dropdown menu for annotations
-
-            // context menu
-            'contextMenuPopup',
-
-            // context menu when selecting text
-            'textPopup',
-
-            // rotation tools in the dropdown settings menu
-            'rotateHeader',
-            'rotateClockwiseButton',
-            'rotateCounterClockwiseButton',
-            'viewControlsDivider1',
-        ],
         initialDoc: 'https://apryse.s3.amazonaws.com/public/files/samples/WebviewerDemoDoc.pdf',
     },
     element,
 );
-const { Core: { annotationManager, documentViewer, Annotations } } = webViewerInstance;
+const { Core: { annotationManager, documentViewer, Annotations }, UI } = webViewerInstance;
+// // const modularHeaders = UI.getModularHeaderList();
+// // console.log(modularHeaders[0].getItems());
+// // UI.setModularHeaders([modularHeaders[0]]);
 
-// wait for document to be loaded
-await new Promise((resolve) => {
-    documentViewer.addEventListener('documentLoaded', resolve);
-});
+// // const viewControls = new UI.Components.ViewControls();
+// // const zoom = new UI.Components.Zoom();
+// // const defaultHeader = UI.getModularHeader('default-top-header');
+// // defaultHeader.setItems([viewControls, zoom]);
+// const viewControls = new UI.Components.ViewControls();
+// // const zoom = new UI.Components.Zoom();
+// const topHeader = new UI.Components.ModularHeader({
+//     dataElement: 'default-top-header',
+//     placement: 'top',
+//     // grow: 0,
+//     // gap: 12,
+//     // position: 'start',
+//     // stroke: true,
+//     // dimension: {
+//     //   paddingTop: 8,
+//     //   paddingBottom: 8,
+//     //   borderWidth: 1
+//     // },
+//     // style: {},
+//     items: [
+//         viewControls,
+//         // zoom,
+//     ]
+//   });
 
-await new Promise((resolve) => setTimeout(resolve, 5000)); // prevent timing issues, otherwise the render is not correct, page is rotate, but dimensions/coordinates are not updated
+//   // Set the modular header in the UI
+// UI.setModularHeaders([topHeader]);
+// UI.enableViewOnlyMode();
+// console.log(UI.exportModularComponents());
+UI.importModularComponents(ui);
 
-// rotate first page counter-clockwise
-const doc = documentViewer.getDocument();
-await doc.rotatePages([1], -1);
+// // wait for document to be loaded
+// await new Promise((resolve) => {
+//     documentViewer.addEventListener('documentLoaded', resolve);
+// });
 
-await new Promise((resolve) => setTimeout(resolve, 5000)); // prevent timing issues, otherwise the render is not correct, page is rotate, but dimensions/coordinates are not updated
+// await new Promise((resolve) => setTimeout(resolve, 5000)); // prevent timing issues, otherwise the render is not correct, page is rotate, but dimensions/coordinates are not updated
 
-// add stamp annotation
-// with random image from picsum.photos
-const res = await fetch('https://picsum.photos/200');
+// // rotate first page counter-clockwise
+// const doc = documentViewer.getDocument();
+// await doc.rotatePages([1], -1);
 
-const imageBlob = await res.blob();
-const reader = new FileReader();
-reader.onloadend = async () => {
-const annot = new Annotations.StampAnnotation({
-    PageNumber: 1,
-    X: 200,
-    Y: 200,
-    Width: 120,
-    Height: 80,
-});
+// await new Promise((resolve) => setTimeout(resolve, 5000)); // prevent timing issues, otherwise the render is not correct, page is rotate, but dimensions/coordinates are not updated
 
-const base64data = reader.result;
-await annot.setImageData(base64data); // Base64 URL or SVG
+// // add stamp annotation
+// // with random image from picsum.photos
+// const res = await fetch('https://picsum.photos/200');
 
-// rotate annotation
-annot.Rotation = -90;
-// flip height and width, because I don't know why
-[annot.Height, annot.Width] = [annot.Width, annot.Height];
+// const imageBlob = await res.blob();
+// const reader = new FileReader();
+// reader.onloadend = async () => {
+// const annot = new Annotations.StampAnnotation({
+//     PageNumber: 1,
+//     X: 200,
+//     Y: 200,
+//     Width: 120,
+//     Height: 80,
+// });
 
-annotationManager.addAnnotation(annot);
-annotationManager.redrawAnnotation(annot);
-}
-reader.readAsDataURL(imageBlob);
+// const base64data = reader.result;
+// await annot.setImageData(base64data); // Base64 URL or SVG
+
+// // rotate annotation
+// annot.Rotation = -90;
+// // flip height and width, because I don't know why
+// [annot.Height, annot.Width] = [annot.Width, annot.Height];
+
+// annotationManager.addAnnotation(annot);
+// annotationManager.redrawAnnotation(annot);
+// }
+// reader.readAsDataURL(imageBlob);
 
 // await new Promise((resolve) => setTimeout(resolve, 5000)); // prevent timing issues
 
---
+++ src/ui3.js
@@ -0,0 +1,395 @@
+export default {
+  "modularComponents": {
+    "view-controls": {
+      "dataElement": "view-controls",
+      "type": "viewControls",
+      "title": "component.viewControls",
+      "icon": "icon-header-page-manipulation-line"
+    },
+    "divider-0.3": {
+      "dataElement": "divider-0.3",
+      "type": "divider"
+    },
+    "zoom-container": {
+      "dataElement": "zoom-container",
+      "type": "zoom"
+    },
+    "divider-0.2": {
+      "dataElement": "divider-0.2",
+      "type": "divider"
+    },
+    "panToolButton": {
+      "dataElement": "panToolButton",
+      "type": "toolButton",
+      "toolName": "Pan"
+    },
+    "annotationEditToolButton": {
+      "dataElement": "annotationEditToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationEdit"
+    },
+    "groupedLeftHeaderButtons": {
+      "dataElement": "groupedLeftHeaderButtons",
+      "items": [
+        "view-controls",
+        "divider-0.3",
+        "zoom-container",
+        "divider-0.2",
+        "panToolButton",
+        "annotationEditToolButton"
+      ],
+      "type": "groupedItems",
+      "grow": 1,
+      "gap": 12,
+      "alwaysVisible": true
+    },
+    "highlightToolButton": {
+      "dataElement": "highlightToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateTextHighlight"
+    },
+    "underlineToolButton": {
+      "dataElement": "underlineToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateTextUnderline"
+    },
+    "strikeoutToolButton": {
+      "dataElement": "strikeoutToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateTextStrikeout"
+    },
+    "squigglyToolButton": {
+      "dataElement": "squigglyToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateTextSquiggly"
+    },
+    "freeTextToolButton": {
+      "dataElement": "freeTextToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateFreeText"
+    },
+    "markInsertTextToolButton": {
+      "dataElement": "markInsertTextToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateMarkInsertText"
+    },
+    "markReplaceTextToolButton": {
+      "dataElement": "markReplaceTextToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateMarkReplaceText"
+    },
+    "freeHandToolButton": {
+      "dataElement": "freeHandToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateFreeHand"
+    },
+    "freeHandHighlightToolButton": {
+      "dataElement": "freeHandHighlightToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateFreeHandHighlight"
+    },
+    "stickyToolButton": {
+      "dataElement": "stickyToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateSticky"
+    },
+    "calloutToolButton": {
+      "dataElement": "calloutToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateCallout"
+    },
+    "divider-0.5": {
+      "dataElement": "divider-0.5",
+      "type": "divider"
+    },
+    "undoButton": {
+      "dataElement": "undoButton",
+      "type": "presetButton",
+      "buttonType": "undoButton"
+    },
+    "redoButton": {
+      "dataElement": "redoButton",
+      "type": "presetButton",
+      "buttonType": "redoButton"
+    },
+    "eraserToolButton": {
+      "dataElement": "eraserToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationEraserTool"
+    },
+    "defaultAnnotationUtilities": {
+      "dataElement": "defaultAnnotationUtilities",
+      "items": [
+        "divider-0.5",
+        "undoButton",
+        "redoButton",
+        "eraserToolButton"
+      ],
+      "type": "groupedItems",
+      "grow": 0,
+      "gap": 12,
+      "alwaysVisible": false
+    }, "rectangleToolButton": {
+      "dataElement": "rectangleToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateRectangle"
+    },
+    "ellipseToolButton": {
+      "dataElement": "ellipseToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateEllipse"
+    },
+    "arcToolButton": {
+      "dataElement": "arcToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateArc"
+    },
+    "polygonToolButton": {
+      "dataElement": "polygonToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreatePolygon"
+    },
+    "cloudToolButton": {
+      "dataElement": "cloudToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreatePolygonCloud"
+    },
+    "lineToolButton": {
+      "dataElement": "lineToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateLine"
+    },
+    "polylineToolButton": {
+      "dataElement": "polylineToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreatePolyline"
+    },
+    "arrowToolButton": {
+      "dataElement": "arrowToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateArrow"
+    },
+    "rubberStampToolButton": {
+      "dataElement": "rubberStampToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateRubberStamp"
+    },
+    "signatureCreateToolButton": {
+      "dataElement": "signatureCreateToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateSignature"
+    },
+    "fileAttachmentButton": {
+      "dataElement": "fileAttachmentButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateFileAttachment"
+    },
+    "stampToolButton": {
+      "dataElement": "stampToolButton",
+      "type": "toolButton",
+      "toolName": "AnnotationCreateStamp"
+    },
+    "signatureFieldButton": {
+      "dataElement": "signatureFieldButton",
+      "type": "toolButton",
+      "toolName": "SignatureFormFieldCreateTool"
+    },
+    "textFieldButton": {
+      "dataElement": "textFieldButton",
+      "type": "toolButton",
+      "toolName": "TextFormFieldCreateTool"
+    },
+    "checkboxFieldButton": {
+      "dataElement": "checkboxFieldButton",
+      "type": "toolButton",
+      "toolName": "CheckBoxFormFieldCreateTool"
+    },
+    "radioFieldButton": {
+      "dataElement": "radioFieldButton",
+      "type": "toolButton",
+      "toolName": "RadioButtonFormFieldCreateTool"
+    },
+    "listBoxFieldButton": {
+      "dataElement": "listBoxFieldButton",
+      "type": "toolButton",
+      "toolName": "ListBoxFormFieldCreateTool"
+    },
+    "comboBoxFieldButton": {
+      "dataElement": "comboBoxFieldButton",
+      "type": "toolButton",
+      "toolName": "ComboBoxFormFieldCreateTool"
+    },
+    "divider-0.7": {
+      "dataElement": "divider-0.7",
+      "type": "divider"
+    },
+    "formFieldEditButton": {
+      "dataElement": "formFieldEditButton",
+      "type": "presetButton",
+      "buttonType": "formFieldEditButton"
+    },
+    "page-controls-container": {
+      "dataElement": "page-controls-container",
+      "type": "pageControls",
+      "title": "component.pageControls",
+      "icon": "icon-page-controls"
+    }
+  },
+  "modularHeaders": {
+    "default-top-header": {
+      "dataElement": "default-top-header",
+      "placement": "top",
+      "grow": 0,
+      "gap": 12,
+      "position": "start",
+      "float": false,
+      "stroke": true,
+      "dimension": {
+        "paddingTop": 8,
+        "paddingBottom": 8,
+        "borderWidth": 1
+      },
+      "style": {},
+      "items": [
+        "groupedLeftHeaderButtons"
+      ]
+    },
+    "page-nav-floating-header": {
+      "dataElement": "page-nav-floating-header",
+      "placement": "bottom",
+      "grow": 0,
+      "gap": 12,
+      "position": "center",
+      "opacityMode": "dynamic",
+      "opacity": "none",
+      "float": true,
+      "stroke": true,
+      "dimension": {
+        "paddingTop": 8,
+        "paddingBottom": 8,
+        "borderWidth": 1
+      },
+      "style": {
+        "background": "var(--gray-1)",
+        "padding": "8px",
+        "borderStyle": "solid",
+        "borderWidth": 1,
+        "borderColor": "var(--gray-5)"
+      },
+      "items": [
+        "page-controls-container"
+      ]
+    }
+  },
+  "panels": {
+
+  },
+  "flyouts": {},
+  "popups": {
+    "annotationPopup": [
+      {
+        "dataElement": "viewFileButton"
+      },
+      {
+        "dataElement": "annotationCommentButton"
+      },
+      {
+        "dataElement": "annotationStyleEditButton"
+      },
+      {
+        "dataElement": "annotationDateEditButton"
+      },
+      {
+        "dataElement": "annotationRedactButton"
+      },
+      {
+        "dataElement": "annotationCropButton"
+      },
+      {
+        "dataElement": "annotationContentEditButton"
+      },
+      {
+        "dataElement": "annotationClearSignatureButton"
+      },
+      {
+        "dataElement": "annotationGroupButton"
+      },
+      {
+        "dataElement": "annotationUngroupButton"
+      },
+      {
+        "dataElement": "formFieldEditButton"
+      },
+      {
+        "dataElement": "calibratePopupButton"
+      },
+      {
+        "dataElement": "linkButton"
+      },
+      {
+        "dataElement": "fileAttachmentDownload"
+      },
+      {
+        "dataElement": "annotationDeleteButton"
+      },
+      {
+        "dataElement": "shortCutKeysFor3D"
+      },
+      {
+        "dataElement": "playSoundButton"
+      },
+      {
+        "dataElement": "openAlignmentButton"
+      }
+    ],
+    "textPopup": [
+      {
+        "dataElement": "copyTextButton"
+      },
+      {
+        "dataElement": "textHighlightToolButton"
+      },
+      {
+        "dataElement": "textUnderlineToolButton"
+      },
+      {
+        "dataElement": "textSquigglyToolButton"
+      },
+      {
+        "dataElement": "textStrikeoutToolButton"
+      },
+      {
+        "dataElement": "textRedactToolButton"
+      },
+      {
+        "dataElement": "linkButton"
+      }
+    ],
+    "contextMenuPopup": [
+      {
+        "dataElement": "panToolButton"
+      },
+      {
+        "dataElement": "stickyToolButton"
+      },
+      {
+        "dataElement": "highlightToolButton"
+      },
+      {
+        "dataElement": "freeHandToolButton"
+      },
+      {
+        "dataElement": "freeHandHighlightToolButton"
+      },
+      {
+        "dataElement": "freeTextToolButton"
+      },
+      {
+        "dataElement": "markInsertTextToolButton"
+      },
+      {
+        "dataElement": "markReplaceTextToolButton"
+      }
+    ]
+  }
+};
